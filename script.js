354f33d5caca83d6f835e19ede1a2c5227c05c41
PEACHG <Nyneicon98@gmail.com>
2022-10-21 07:29:54 -0400

    Add Home screen, needs details
    Game requires scripting.

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
 let playerScore = 0;
 let computerScore = 0;
+const computerRock = "rock";
+const computerPaper = "paper";
+const computerScissors = "scissors";
 const rock = document.getElementsByClassName("rockTwo");
 const paper = document.getElementsByClassName("paperTwo");
 const scissors = document.getElementsByClassName("scissorsTwo");
-const choice = 
-
+const choice = document.getElementsByClassName("buttons")
 
 
 function getComputerChoice() {
-    let choices = [rock, paper, scissors];
+    let choices = [computerRock, computerPaper, computerScissors];
     let choice = choices[Math.floor(Math.random() * choices.length)]; 
     return choice.toString();
 }
 
-function battleBox() {
-    if (document.getElementsByClassName("buttons").addEventListener("click")) {
-        document.getElementsByClassName("battleBox").style.visibility ="visible";
-    }
-}
+
 
 function playRound() {
-    playerSelection = getPlayerChoice(); /* Causes issues with if function below. Replace with "rock" for testing*/
+
+    let playerSelection = ""
+
+    function getPlayerChoice(){
+        if (element.classList.contains("rockTwo")) {
+            playerSelection = "rock";
+            return playerSelection.toString()
+        } else if (element.classList.contains("paperTwo")) {
+            playerSelection = "paper";
+            return playerSelection.toString();
+        } else if (element.classList.contains("scissorsTwo")) {
+            playerSelection = "scissors";
+            return playerSelection.toString();
+        } 
+    }
+
+    playerSelection = getPlayerChoice();
     computerSelection = getComputerChoice();
     alert(`You chose ${playerSelection}.\nYour opponent chose ${computerSelection}.`); 
 
     if ((playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")) {
-        alert("You win\nYour Score: " + (++playerScore));   
+        alert("You win");   
     }
     else if (playerSelection === computerSelection) {
-        alert("Its a draw\nYour Score: " + (playerScore) + "\nOpponent's Score: " + (computerScore));   
+        alert("Its a draw");   
     }
     else {
-        alert("You lose\nOpponent's Score: " + (++computerScore));
+        alert("You lose");
     }
 }
 
-function playGame() {
-    for(i = 0; i < 100; i++) {
-        playRound(i);
-        
-        if (playerScore == 3) {
-            return "YOU HAVE WON THE GAME! Your Score: " + (playerScore);
-        } else if (computerScore == 3) {
-            return "YOUR OPPONENT HAS WON THE GAME. Opponent's Score: " + (computerScore);
-        }
-    }
-
-    }
