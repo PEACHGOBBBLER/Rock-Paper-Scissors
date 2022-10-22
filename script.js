1fb56460992f0d4137fe229c3ea30a024a6d7a9a
PEACHG <Nyneicon98@gmail.com>
2022-10-22 01:48:23 -0400

    Button functionality
    Value of button printed and compareed to computer's random result

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let computerScore = 0;
 const computerRock = "rock";
 const computerPaper = "paper";
 const computerScissors = "scissors";
-const rock = document.getElementsByClassName("rockTwo");
-const paper = document.getElementsByClassName("paperTwo");
-const scissors = document.getElementsByClassName("scissorsTwo");
-const choice = document.getElementsByClassName("buttons")
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+
 
 
 function getComputerChoice() {
@@ -17,24 +17,9 @@ function getComputerChoice() {
 
 
 
-function playRound() {
+function playRound(val) {
 
-    let playerSelection = ""
-
-    function getPlayerChoice(){
-        if (element.classList.contains("rockTwo")) {
-            playerSelection = "rock";
-            return playerSelection.toString()
-        } else if (element.classList.contains("paperTwo")) {
-            playerSelection = "paper";
-            return playerSelection.toString();
-        } else if (element.classList.contains("scissorsTwo")) {
-            playerSelection = "scissors";
-            return playerSelection.toString();
-        } 
-    }
-
-    playerSelection = getPlayerChoice();
+    let playerSelection = val;
     computerSelection = getComputerChoice();
     alert(`You chose ${playerSelection}.\nYour opponent chose ${computerSelection}.`); 
 
